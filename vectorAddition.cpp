74b11b45119bbbaac273269d086a7c9c761345da
John Nguyen <37886033+FireShredder24@users.noreply.github.com>
2021-04-15 21:38:09 -0700

    Remove unnecessary reference angle calculations

--- vectorAddition.cpp
+++ vectorAddition.cpp
@@ -24,25 +24,11 @@ int main() {
 	float dir2;
 	cin >> dir2;
 	if (dir1 < 360 && dir2 < 360 && dir1 >= 0 && dir2 >= 0) {
-		// Determines which quadrant the angle is in, then generates a reference angle
-		bool cosPositive1 = (dir1 <= 90 || dir1 >= 270) ? true : false;
-		bool sinPositive1 = (dir1 <= 180) ? true : false;
-		float ref1 = (cosPositive1) ? (sinPositive1) ? dir1 : 360 - dir1 : (sinPositive1) ? 180 - dir1 : 270 - dir1;
-		cout << "Reference angle of first vector: " << ref1 << " Cosine positive? " << cosPositive1 << " Sine positive? " << sinPositive1;
-		// Converts into radians to simplify trig functions
-		ref1 *= degreesToRad;
-		// Determines which quadrant the angle is in, then generates a reference angle
-		bool cosPositive2 = (dir2 <= 90 || dir2 >= 270) ? true : false;
-		bool sinPositive2 = (dir2 <= 180) ? true : false;
-		float ref2 = (cosPositive2) ? (sinPositive2) ? dir2 : 360 - dir2 : (sinPositive2) ? 180 - dir2 : 270 - dir2;
-		cout << "\nReference angle of second vector: " << ref2 << " Cosine positive? " << cosPositive2 << " Sine positive? " << sinPositive2;
-		// Converts into radians to simplify trig functions
-		ref2 *= degreesToRad;
 		// Generates components of vectors and prints them
-		float x1 = mag1 * cos(dir1 * degreesToRad); //* ((cosPositive1) ? 1 : -1);
-		float y1 = mag1 * sin(dir1 * degreesToRad); //* ((sinPositive1) ? 1 : -1);
-		float x2 = mag2 * cos(dir2 * degreesToRad); //* ((cosPositive2) ? 1 : -1);
-		float y2 = mag2 * sin(dir2 * degreesToRad); //* ((sinPositive2) ? 1 : -1);
+		float x1 = mag1 * cos(dir1 * degreesToRad);
+		float y1 = mag1 * sin(dir1 * degreesToRad);
+		float x2 = mag2 * cos(dir2 * degreesToRad);
+		float y2 = mag2 * sin(dir2 * degreesToRad);
 		cout << "\nVector 1: <" << x1 << ", " << y1 << ">";
 		cout << "\nVector 2: <" << x2 << ", " << y2 << ">";
 		// Generates components of resultant vector
